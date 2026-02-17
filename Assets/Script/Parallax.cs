e6e35222cd2b9fbcce1011075233a83c32b0538c
Minecoal <michaelxu1201@gmail.com>
2026-02-17 12:18:27 -0800

    light fixes
    fix lighting issue on walls

--- Assets/Script/Parallax.cs
+++ Assets/Script/Parallax.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 
 public class ParallaxLayer : MonoBehaviour
 {
-    public float totalLayers;
-    private float factor; // how much this layer moves / darkens
+    public int totalLayers;
+    private int factor; // how much this layer moves / darkens
 
     [SerializeField] private WallUpdater[] childWalls;
     private Vector3 originalPosition;
@@ -12,42 +12,49 @@ public class ParallaxLayer : MonoBehaviour
     private List<Vector3> originalLocalScales;
 
     void Awake()
-{
-    childWalls = GetComponentsInChildren<WallUpdater>();
-
-    originalLocalPositions = new List<Vector3>();
-    originalLocalScales = new List<Vector3>();
-
-    foreach (WallUpdater wall in childWalls)
     {
-        originalLocalPositions.Add(wall.transform.localPosition);
-        originalLocalScales.Add(wall.transform.localScale);
+        childWalls = GetComponentsInChildren<WallUpdater>();
+
+        originalLocalPositions = new List<Vector3>();
+        originalLocalScales = new List<Vector3>();
+        foreach (WallUpdater wall in childWalls)
+        {
+            originalLocalPositions.Add(wall.transform.localPosition);
+            originalLocalScales.Add(wall.transform.localScale);
+        }
     }
-}
+
+    void Start()
+    {
+        foreach (WallUpdater wall in childWalls)
+        {
+            wall.SetRenderOrder(factor);
+        }
+    }
 
     public void SetFactor(int factor)
     {
         this.factor = factor;
     }
 
-    public void ApplyTint()
+    public void ApplyTint(float maxTint)
     {
         if (childWalls == null) return;
         foreach (WallUpdater wall in childWalls)
         {
-            wall.SetTint(1 - (factor / (totalLayers - 1f)));
+            wall.SetTint(factor / (totalLayers - 1f), maxTint);
         }
     }
 
-    public void UpdateLayerPosition(Vector3 playerPosition, float skewAmount, float scaleAmount)
+    public void UpdateLayerPosition(Vector3 playerPosition, float skewAmountx, float skewAmounty, float scaleAmount)
     {
         for (int i = 0; i < childWalls.Length; i++)
         {
             WallUpdater t = childWalls[i];
 
             t.transform.position = originalLocalPositions[i] + new Vector3( 
-                (originalLocalPositions[i].x - playerPosition.x) * factor * skewAmount,
-                (originalLocalPositions[i].y - playerPosition.y) * factor * skewAmount,
+                (originalLocalPositions[i].x - playerPosition.x) * factor * skewAmountx,
+                (originalLocalPositions[i].y - playerPosition.y) * factor * skewAmounty,
                 0f
             );
 
@@ -55,15 +62,12 @@ public class ParallaxLayer : MonoBehaviour
         }
     }
     
-    public void Clear()
+    public void Init(UnityEngine.Rendering.Universal.ShadowCaster2D.ShadowCastingOptions type, bool clear)
     {
-        int i = 0;
         foreach (WallUpdater wall in childWalls)
         {
-            wall.SetRenderOrder(-i);
-            wall.Clear();
-            wall.SetCastType();
-            i++;
+            if (clear) wall.Clear();
+            wall.SetCastType(type);
         }
     }
 }
--- Assets/Script/WallUpdater.cs
+++ Assets/Script/WallUpdater.cs
@@ -8,6 +8,7 @@ public class WallUpdater : MonoBehaviour
     private ShadowCaster2D shadowCaster;
     private Collider2D collider2d;
     private NavMeshObstacle obstacle;
+    Color baseColor;
 
     void Awake()
     {
@@ -15,16 +16,17 @@ public class WallUpdater : MonoBehaviour
         shadowCaster = GetComponentInChildren<ShadowCaster2D>();
         collider2d = GetComponentInChildren<Collider2D>();
         obstacle = GetComponentInChildren<NavMeshObstacle>();
+        baseColor = sr.color;
     }
 
-    public void SetTint(float valueFactor)
+    public void SetTint(float valueFactor, float maxTint)
     {
         if (sr == null) return;
         
         Color c = sr.color;
-        c.r = c.r * valueFactor;
-        c.g = c.g * valueFactor;
-        c.b = c.b * valueFactor;
+        c.r = Mathf.Lerp(baseColor.r, 1 - maxTint, valueFactor);
+        c.g = Mathf.Lerp(baseColor.g, 1 - maxTint, valueFactor);
+        c.b = Mathf.Lerp(baseColor.b, 1 - maxTint, valueFactor);
         sr.color = c;
     }
 
@@ -32,16 +34,16 @@ public class WallUpdater : MonoBehaviour
     {
         Destroy(collider2d);
         Destroy(obstacle);
-        // Destroy(shadowCaster);
     }
 
-    public void SetCastType()
+    public void SetCastType(ShadowCaster2D.ShadowCastingOptions type)
     {
-        shadowCaster.castingOption = ShadowCaster2D.ShadowCastingOptions.CastShadow;
+        shadowCaster.castingOption = type;
     }
 
     public void SetRenderOrder(int i)
     {
-        sr.rendererPriority = i;
+        Debug.Log(i);
+        sr.sortingOrder = i;
     }
 }
